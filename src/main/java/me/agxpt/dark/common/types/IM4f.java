9ff0de7f72fe104d3f98fcda8ba2a7ad43dcfb12
BuildTools <85099955+agxpt@users.noreply.github.com>
2022-04-14 15:00:05 +0430

    Add IM4f

---
+++ src/main/java/me/agxpt/dark/common/types/IM4f.java
@@ -0,0 +1,12 @@
+package me.agxpt.dark.common.types;
+
+import java.nio.FloatBuffer;
+
+public interface IM4f {
+    /**
+     * Get the matrix as a float buffer.
+     *
+     * @return The matrix as a float buffer.
+     */
+    FloatBuffer getAsBuffer();
+}
